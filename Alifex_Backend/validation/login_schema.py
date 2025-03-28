2e53b8ad91d927f3d7845465514cb2f647557ec4
Leah159 <164363046+Leah159@users.noreply.github.com>
2025-03-28 13:06:01 +0000

    Delete Alifex_Backend directory

--- Alifex_Backend/validation/login_schema.py
+++
@@ -1,9 +0,0 @@
-"""
-A module to handle the schemas for the login endpoint
-"""
-
-login_post_schema = {
-    "type": "object",
-    "properties": {"email": {"type": "string"}, "password": {"type": "string"}},
-    "required": ["email", "password"]
-}
